e205fd9c3a64154f4856bd9ed0e808f83455f5a7
Documenter.jl <documenter@juliadocs.github.io>
2021-08-01 22:41:58 +0000

    build based on 38a9625

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeometricInterpretation","category":"page"},{"location":"#GeometricInterpretation","page":"Home","title":"GeometricInterpretation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeometricInterpretation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeometricInterpretation]","category":"page"}]
+}
